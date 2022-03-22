d2502db4d1158349f92ea573274a6ee9a1ab2ee7
Kyle Chapman <kyle.chapman@durhamcollege.ca>
2022-03-22 15:02:57 -0400

    Add project files.

---
+++ Airlines.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace Airlines
+{
+    partial class Airlines
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBoxAirlines = new System.Windows.Forms.ListBox();
+            this.textBoxAirlineEntry = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.buttonSort = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonImport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxAirlines
+            // 
+            this.listBoxAirlines.FormattingEnabled = true;
+            this.listBoxAirlines.ItemHeight = 20;
+            this.listBoxAirlines.Items.AddRange(new object[] {
+            "Air Canada",
+            "Qantas"});
+            this.listBoxAirlines.Location = new System.Drawing.Point(43, 78);
+            this.listBoxAirlines.Name = "listBoxAirlines";
+            this.listBoxAirlines.Size = new System.Drawing.Size(246, 244);
+            this.listBoxAirlines.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.listBoxAirlines, "It\'s a list of airlines");
+            // 
+            // textBoxAirlineEntry
+            // 
+            this.textBoxAirlineEntry.Location = new System.Drawing.Point(43, 32);
+            this.textBoxAirlineEntry.Name = "textBoxAirlineEntry";
+            this.textBoxAirlineEntry.Size = new System.Drawing.Size(125, 27);
+            this.textBoxAirlineEntry.TabIndex = 0;
+            this.toolTip1.SetToolTip(this.textBoxAirlineEntry, "Enter an airline here");
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(195, 32);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(94, 29);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = "&Add";
+            this.toolTip1.SetToolTip(this.buttonAdd, "Click to add the airline to the list");
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(43, 336);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(94, 29);
+            this.buttonRemove.TabIndex = 3;
+            this.buttonRemove.Text = "&Remove";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // buttonSort
+            // 
+            this.buttonSort.Location = new System.Drawing.Point(195, 336);
+            this.buttonSort.Name = "buttonSort";
+            this.buttonSort.Size = new System.Drawing.Size(94, 29);
+            this.buttonSort.TabIndex = 4;
+            this.buttonSort.Text = "&Sort";
+            this.toolTip1.SetToolTip(this.buttonSort, "Sorts the list");
+            this.buttonSort.UseVisualStyleBackColor = true;
+            this.buttonSort.Click += new System.EventHandler(this.buttonSort_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(43, 383);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(94, 29);
+            this.buttonSave.TabIndex = 5;
+            this.buttonSave.Text = "Sa&ve";
+            this.toolTip1.SetToolTip(this.buttonSave, "Saves the list to a selected location");
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.SaveClick);
+            // 
+            // buttonImport
+            // 
+            this.buttonImport.Location = new System.Drawing.Point(195, 383);
+            this.buttonImport.Name = "buttonImport";
+            this.buttonImport.Size = new System.Drawing.Size(94, 29);
+            this.buttonImport.TabIndex = 6;
+            this.buttonImport.Text = "&Import";
+            this.toolTip1.SetToolTip(this.buttonImport, "Replaces the current list with one from a file");
+            this.buttonImport.UseVisualStyleBackColor = true;
+            this.buttonImport.Click += new System.EventHandler(this.ImportClick);
+            // 
+            // Airlines
+            // 
+            this.AcceptButton = this.buttonAdd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333, 436);
+            this.Controls.Add(this.buttonImport);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonSort);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxAirlineEntry);
+            this.Controls.Add(this.listBoxAirlines);
+            this.Name = "Airlines";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Airline List";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxAirlines;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TextBox textBoxAirlineEntry;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Button buttonSort;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonImport;
+    }
+}
+
---
+++ Airlines.cs
@@ -0,0 +1,175 @@
+﻿// Author: Kyle Chapman
+// Date: March 22, 2022
+// Description:
+// This does stuff and is incomplete.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Airlines
+{
+    public partial class Airlines : Form
+    {
+        List<string> airlineList = new List<string> { "Air Canada", "Qantas" };
+
+        public Airlines()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Attempts to add the contents of the textbox on the form into the list(box?).
+        /// </summary>
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            // If the ListBox already includes the airline, report an error and don't add it to the list.
+            if (listBoxAirlines.Items.Contains(textBoxAirlineEntry.Text.Trim()))
+            {
+                MessageBox.Show(textBoxAirlineEntry.Text + " is already on the list of airlines.");
+            }
+            // If the textbox is blank, report an error and don't add anything.
+            else if (textBoxAirlineEntry.Text.Trim() == String.Empty)
+            {
+                MessageBox.Show("You must enter some kind of text for the airline.");
+            }
+            // If the airline is new and non-empty, add it to the list.
+            else
+            {
+                airlineList.Add(textBoxAirlineEntry.Text.Trim());
+                listBoxAirlines.Items.Add(textBoxAirlineEntry.Text.Trim());
+                textBoxAirlineEntry.Clear(); 
+            }    
+        }
+
+        /// <summary>
+        /// Attempt to remove the selected item from the listBox.
+        /// </summary>
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            if (listBoxAirlines.SelectedIndex >= 0)
+            {
+                airlineList.RemoveAt(listBoxAirlines.SelectedIndex);
+                listBoxAirlines.Items.Remove(listBoxAirlines.SelectedItem);
+            }
+            else
+            {
+                MessageBox.Show("Select an airline to remove.");
+            }
+            
+        }
+
+        /// <summary>
+        /// This sorts our local list of airlines, clears the ListBox, and then adds our airlines into the ListBox one-by-one.
+        /// </summary>
+        private void buttonSort_Click(object sender, EventArgs e)
+        {
+            airlineList.Sort();
+            listBoxAirlines.Items.Clear();
+
+            foreach (string airline in airlineList)
+            {
+                listBoxAirlines.Items.Add(airline);
+            }
+        }
+
+        /// <summary>
+        /// Saves the current list to a text file in a default location.
+        /// </summary>
+        private void SaveClick(object sender, EventArgs e)
+        {
+            // Declare, instantiate and configure a new SaveFileDialog.
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.FileName = "List" + DateTime.Now.ToString("yyyyMMdd") + ".alf";
+            saveDialog.Filter = "Airline Lists (*.alf)|*.alf|Comma separated value (*.csv)|*.csv|Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            
+            // If the person picks a file and clicks "OK", continue processing.
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Create some System.IO objects to make file access easier.
+                FileStream fileToAccess = new FileStream(saveDialog.FileName, FileMode.Create, FileAccess.Write);
+                StreamWriter writer = new StreamWriter(fileToAccess);
+
+                // If the filetype is .csv, prepare it as a comma separated value.
+                if (saveDialog.FilterIndex == 2)
+                {
+                    // Count through the list
+                    for (int index = 0; index < airlineList.Count; index++)
+                    {
+                        // Write the current item in the list into the file.
+                        writer.Write(airlineList[index].ToString() + ", value!, something" + "\n");
+                    }
+                }
+                // If the filetype ISN'T .csv.
+                else
+                {
+                    // Count through the list
+                    for (int index = 0; index < airlineList.Count; index++)
+                    {
+                        // Write the current item in the list into the file.
+                        writer.Write(airlineList[index].ToString() + "\n");
+                    }
+                }
+
+                // Close the file.
+                writer.Close();
+
+                MessageBox.Show("Your work is saved to " + saveDialog.FileName);
+            }
+            // The person didn't pick a file.
+            else
+            {
+                MessageBox.Show("We didn't save anything. :(");
+            }
+        }
+
+        /// <summary>
+        /// This will prompt the user to confirm they want to overwrite their list, then it picks a file and overwrites their list.
+        /// </summary>
+        private void ImportClick(object sender, EventArgs e)
+        {
+            // Confirm that you want to overwrite.
+            if (MessageBox.Show("This action will overwrite your current list. Are you sure?", "Overwrite Data?", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                // Make a new OpenFileDialog and set its filter.
+                OpenFileDialog openDialog = new OpenFileDialog();
+                openDialog.Filter = "Airline Lists (*.alf)|*.alf";
+
+                // If the user has picked a file...
+                if (openDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Declare System.IO objects to write to that file.
+                    FileStream fileToAccess = new FileStream(openDialog.FileName, FileMode.Open, FileAccess.Read);
+                    StreamReader reader = new StreamReader(fileToAccess);
+
+                    // Clear the lists.
+                    airlineList.Clear();
+                    listBoxAirlines.Items.Clear();
+
+                    // Read until the end of the file and fill those lines into the list.
+                    while (!reader.EndOfStream)
+                    {
+                        string newAirline = reader.ReadLine();
+
+                        airlineList.Add(newAirline);
+                        listBoxAirlines.Items.Add(newAirline);
+                    }
+
+                    reader.Close();
+                }
+            }
+            // Confirm with the user if they don't want to overwrite their list.
+            else
+            {
+                MessageBox.Show("Okay, relax. We're not doing it.");
+            }
+        }
+    }
+}
